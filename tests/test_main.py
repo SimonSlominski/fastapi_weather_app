6f89e53ed9a7f2e63a0e7588675f78d05efdab74
Szymon Słomiński <simon.slominski@gmail.com>
2023-08-15 22:55:33 +0200

    Add test for valid city forecasts

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,14 @@
 from fastapi.testclient import TestClient
+
 from app.main import app
 
-
 client = TestClient(app)
+
+
+def test_valid_city_forecasts():
+    response = client.get("/forecasts?city=New York")
+    assert response.status_code == 200
+    data = response.json()
+    assert isinstance(data, dict)
+    assert "2023-08-19" in data
+    assert "min_temp" in data["2023-08-19"]

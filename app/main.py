459d0d46c7a09ee13c86567435ad8e3383181e1e
Szymon Słomiński <simon.slominski@gmail.com>
2023-08-15 13:03:07 +0200

    Update get_weather_forecasts function to retrieve weather forecasts data

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import requests
 
 from geopy.geocoders import Nominatim
 
@@ -12,8 +13,21 @@ app = FastAPI()
 def get_forecasts(city: str):
     pass
 
-def get_weather_forecasts():
-    pass
+def get_weather_forecasts(latitude, longitude):
+    url = f"https://api.open-meteo.com/v1/forecast"
+    params = {
+        "latitude": latitude,
+        "longitude": longitude,
+        "daily": "temperature_2m_max,temperature_2m_min,rain_sum,windspeed_10m_max",
+        "timezone": "auto",
+        "forecast_days": 5
+    }
+
+    response = requests.get(url, params=params)
+    data = response.json()
+    print(data)
+
+
 
 def get_coordinates(city):
     geolocator = Nominatim(user_agent="get_coordinates")
@@ -32,5 +46,6 @@ def get_coordinates(city):
 
 
 if __name__ == "__main__":
-    print(get_coordinates("Berlin"))
-    print(get_coordinates("Wurzburg"))
+    coordinates = get_coordinates("Wurzburg")
+    print(coordinates)
+    print(get_weather_forecasts(49.79, 9.93))

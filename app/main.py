eebea2a16102b44a6b3cf9fcd242b5e82193313c
Szymon Słomiński <simon.slominski@gmail.com>
2023-08-15 12:42:43 +0200

    Add geolocation to convert city names into coordinates

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 
+from geopy.geocoders import Nominatim
+
 
 app = FastAPI()
 
@@ -12,3 +14,23 @@ def get_forecasts(city: str):
 
 def get_weather_forecasts():
     pass
+
+def get_coordinates(city):
+    geolocator = Nominatim(user_agent="get_coordinates")
+
+    try:
+        location = geolocator.geocode(city)
+        if location:
+            latitude = location.latitude
+            longitude = location.longitude
+            return latitude, longitude
+        else:
+            return None
+    except Exception as e:
+        print("Error:", e)
+        return None
+
+
+if __name__ == "__main__":
+    print(get_coordinates("Berlin"))
+    print(get_coordinates("Wurzburg"))
